176cfa3c848433abc2f4bc45d791e40d15b18cdf
Cristian Ninotti <cristianninotti03@gmail.com>
2024-10-18 22:51:33 -0300

    Agregar archivos de proyecto.

---
+++ Web/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(new List<User> {
+
+                            new User {
+                                Id =1, 
+                                UserName="Fana",
+                                Password="asd",
+                                FirstName="Cristian", 
+                                LastName="Ninotti", 
+                                DNI=34732713,
+                                Email="cristianninotti03@gmail.com"
+                                }
+            });
+        }
+    }
+}
