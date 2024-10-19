74a9951752f6665bc7031d89722edbac233ac5a1
Cristian Ninotti <cristianninotti03@gmail.com>
2024-10-19 20:52:03 -0300

    Errores de mapping.

--- Domain/Interfaces/IProfessionalRepository.cs
+++ Domain/Interfaces/IProfessionalRepository.cs
@@ -1,10 +1,5 @@
 ﻿using Domain.Entities;
 using Domain.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Interfaces
 {
