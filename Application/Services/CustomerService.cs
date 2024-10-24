97bbbc255e2bba02e19bcf85720e61eadc00b6fc
Juan Ignacio Zucco <juani_z29@hotmail.com>
2024-10-24 15:24:56 -0300

    corrección put de customer

--- Application/Services/CustomerService.cs
+++ Application/Services/CustomerService.cs
@@ -58,10 +58,44 @@ namespace Application.Services
 
             if (customerEntity != null)
             {
-                CustomerProfile.ToCustomerEntityUpdate(customerEntity, customer);
+
+                if (!string.IsNullOrEmpty(customer.UserName) && customer.UserName != "string")
+                {
+                    customerEntity.UserName = customer.UserName;
+                }
+
+                if (!string.IsNullOrEmpty(customer.Password) && customer.Password != "string")
+                {
+                    customerEntity.Password = customer.Password;
+                }
+
+                if (!string.IsNullOrEmpty(customer.FirstName) && customer.FirstName != "string")
+                {
+                    customerEntity.FirstName = customer.FirstName;
+                }
+
+                if (!string.IsNullOrEmpty(customer.LastName) && customer.LastName != "string")
+                {
+                    customerEntity.LastName = customer.LastName;
+                }
+
+                if (customer.Dni != 0)
+                {
+                    customerEntity.Dni = customer.Dni;
+                }
+
+                if (!string.IsNullOrEmpty(customer.Email) && customer.Email != "string")
+                {
+                    customerEntity.Email = customer.Email;
+                }
+
+
+                if (customer.TypeCustomer != customer.TypeCustomer)
+                {
+                    customerEntity.TypeCustomer = customer.TypeCustomer;
+                }
 
                 _customerRepository.UpdateCustomer(customerEntity);
-
                 return true;
             }
 
